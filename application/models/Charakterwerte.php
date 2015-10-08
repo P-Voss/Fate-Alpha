94994ab5fc24b7889fcda88d241d56483d481c7c
P-Voss <voss.ph@web.de>
2015-10-08 21:26:21 +0200

    Charakterprofil verschönert

--- application/models/Charakterwerte.php
+++ application/models/Charakterwerte.php
@@ -7,77 +7,137 @@
  */
 class Application_Model_Charakterwerte {
     
-    protected $_staerke;
-    protected $_agilitaet;
-    protected $_ausdauer;
-    protected $_disziplin;
-    protected $_kontrolle;
-    protected $_uebung;
-    protected $_fp;
-    protected $_startpunkte;
+    protected $staerke;
+    protected $agilitaet;
+    protected $ausdauer;
+    protected $disziplin;
+    protected $kontrolle;
+    protected $uebung;
+    protected $fp;
+    protected $startpunkte;
     
     public function getStaerke() {
-        return $this->_staerke;
+        return $this->staerke;
     }
 
     public function getAgilitaet() {
-        return $this->_agilitaet;
+        return $this->agilitaet;
     }
 
     public function getAusdauer() {
-        return $this->_ausdauer;
+        return $this->ausdauer;
     }
 
     public function getDisziplin() {
-        return $this->_disziplin;
+        return $this->disziplin;
     }
 
     public function getKontrolle() {
-        return $this->_kontrolle;
+        return $this->kontrolle;
     }
 
     public function getUebung() {
-        return $this->_uebung;
+        return $this->uebung;
     }
 
     public function getFp() {
-        return $this->_fp;
+        return $this->fp;
     }
 
     public function getStartpunkte() {
-        return $this->_startpunkte;
+        return $this->startpunkte;
     }
 
     public function setStaerke($staerke) {
-        $this->_staerke = $staerke;
+        $this->staerke = $staerke;
     }
 
     public function setAgilitaet($agilitaet) {
-        $this->_agilitaet = $agilitaet;
+        $this->agilitaet = $agilitaet;
     }
 
     public function setAusdauer($ausdauer) {
-        $this->_ausdauer = $ausdauer;
+        $this->ausdauer = $ausdauer;
     }
 
     public function setDisziplin($disziplin) {
-        $this->_disziplin = $disziplin;
+        $this->disziplin = $disziplin;
     }
 
     public function setKontrolle($kontrolle) {
-        $this->_kontrolle = $kontrolle;
+        $this->kontrolle = $kontrolle;
     }
 
     public function setUebung($uebung) {
-        $this->_uebung = $uebung;
+        $this->uebung = $uebung;
     }
 
     public function setFp($fp) {
-        $this->_fp = $fp;
+        $this->fp = $fp;
     }
 
     public function setStartpunkte($startpunkte) {
-        $this->_startpunkte = $startpunkte;
+        $this->startpunkte = $startpunkte;
+    }
+    
+    public function getCategory($value) {
+        switch (true) {
+            case $value > 660:
+                $category = "A+";
+                break;
+            case $value > 600:
+                $category = "A";
+                break;
+            case $value > 540:
+                $category = "A-";
+                break;
+            case $value > 480:
+                $category = "B+";
+                break;
+            case $value > 430:
+                $category = "B";
+                break;
+            case $value > 380:
+                $category = "B-";
+                break;
+            case $value > 330:
+                $category = "C+";
+                break;
+            case $value > 290:
+                $category = "C";
+                break;
+            case $value > 250:
+                $category = "C-";
+                break;
+            case $value > 210:
+                $category = "D+";
+                break;
+            case $value > 180:
+                $category = "D";
+                break;
+            case $value > 150:
+                $category = "D-";
+                break;
+            case $value > 120:
+                $category = "E+";
+                break;
+            case $value > 80:
+                $category = "E";
+                break;
+            case $value > 40:
+                $category = "E-";
+                break;
+            case $value > 0:
+                $category = "F+";
+                break;
+            case $value == 0:
+                $category = "F";
+                break;
+            default:
+                $category = "F-";
+                break;
+        }
+        return $category;
     }
     
 }
