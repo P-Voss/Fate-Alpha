b0ee70f9024872b88fc1eac93b24e4d8d60ebc60
P-Voss <voss.ph@web.de>
2018-07-14 13:54:33 +0200

    Bugfix News laden

--- application/models/mappers/NewsMapper.php
+++ application/models/mappers/NewsMapper.php
@@ -37,22 +37,20 @@ class Application_Model_Mapper_NewsMapper
         $result = $this->getDbTable('News')->fetchAll($select);
 
         $usermapper = new Application_Model_Mapper_UserMapper();
-        if ($result->count() > 0) {
-            foreach ($result as $row) {
-                $model = new Application_Model_News();
-                $model->setId($row->newsId);
-                $model->setDatum($row->creationDate);
-                $model->setEditdatum($row->editDate);
-                $model->setEditor($row->editUserId);
-                $model->setNachricht($row->nachricht);
-                $model->setTitel($row->titel);
-                $model->setVerfasser($row->verfasserUserId);
-                $model->setVerfasserName($usermapper->getAdminnameById($row->verfasserUserId));
-                if ($row->editUserId !== null) {
-                    $model->setEditorName($usermapper->getAdminnameById($row->editUserId));
-                }
-                $return[] = $model;
+        foreach ($result as $row) {
+            $model = new Application_Model_News();
+            $model->setId($row->newsId);
+            $model->setDatum($row->creationDate);
+            $model->setEditdatum($row->editDate);
+            $model->setEditor($row->editUserId);
+            $model->setNachricht($row->nachricht);
+            $model->setTitel($row->titel);
+            $model->setVerfasser($row->verfasserUserId);
+            $model->setVerfasserName($usermapper->getAdminnameById($row->verfasserUserId));
+            if ((int)$row->editUserId > 0) {
+                $model->setEditorName($usermapper->getAdminnameById($row->editUserId));
             }
+            $return[] = $model;
         }
         return $return;
     }
