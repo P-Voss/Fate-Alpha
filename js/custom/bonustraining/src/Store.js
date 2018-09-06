66e0e1f61251571a11498d41bb57e142e4a494c5
P-Voss <voss.ph@web.de>
2018-09-06 21:40:29 +0200

    setup deployment for bonustraining app

--- js/custom/bonustraining/src/Store.js
+++ js/custom/bonustraining/src/Store.js
@@ -1,21 +1,20 @@
 import {createStore, applyMiddleware} from "redux";
-import {createLogger} from "redux-logger";
 import thunkMiddleware from "redux-thunk";
-
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 import trainingApp from "./reducers/Reducer";
 
-const loggerMiddleware = createLogger()
+
+// import {createLogger} from "redux-logger";
+// import {composeWithDevTools} from "redux-devtools-extension";
+// let middleware = applyMiddleware(thunkMiddleware);
+// if (process.env.REACT_APP_ENV === "development") {
+//     const loggerMiddleware = createLogger();
+//     middleware = composeWithDevTools(applyMiddleware(thunkMiddleware, loggerMiddleware));
+// }
 
 const store = createStore(
     trainingApp,
-    composeWithDevTools(
-        applyMiddleware(
-            thunkMiddleware, // lets us dispatch() functions
-            loggerMiddleware, // neat middleware that logs actions
-        )
-    )
+    applyMiddleware(thunkMiddleware)
 );
 
 export default store;
