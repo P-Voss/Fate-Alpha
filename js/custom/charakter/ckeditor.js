df1073b5ecfa226b48f4d2856e92c42f8776f65b
P-Voss <voss.ph@web.de>
2019-02-14 22:09:14 +0100

    switched to ckeditor in character profile

---
+++ js/custom/charakter/ckeditor.js
@@ -0,0 +1,11 @@
+
+jQuery(document).ready(function () {
+    CKEDITOR.inline('childhoodEditor');
+    CKEDITOR.inline('raisedEditor');
+    CKEDITOR.inline('eventEditor');
+    CKEDITOR.inline('gainedEditor');
+    CKEDITOR.inline('focusEditor');
+    CKEDITOR.inline('story');
+    CKEDITOR.inline('private');
+
+});
